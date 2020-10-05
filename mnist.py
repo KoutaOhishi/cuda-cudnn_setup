0440cca4f720d9f6c78ab569496408c8f83a4707
KoutaOhishi <bigbasketball568@yahoo.co.jp>
2020-10-05 17:38:39 +0900

    fix source code

--- mnist.py
+++ mnist.py
@@ -1,24 +1,21 @@
 #!/usr/bin/env python
 #coding:utf-8
-#https://qiita.com/Bashi50/items/9ff364f578df726f4c98
-import tensorflow
-from tensorflow import keras
-#matplotlib inline
-import matplotlib.pyplot as plt
+#https://keita-blog.com/data_science/keras-tensorflow-gpu
+import tensorflow as tf
+mnist = tf.keras.datasets.mnist
 
-# Mnistをダウンロードして分けてくれる
-(x_train, y_train),(x_test, y_test) = keras.datasets.mnist.load_data()
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
 
-# 正規化 小数にするので.0忘れずに
-x_train, x_test = x_train / 255.0 , x_test / 255.0
-
-model = keras.models.Sequential([
-    keras.layers.Flatten(),
-    keras.layers.Dense(512, activation='relu'),
-    keras.layers.Dropout(0.2),
-    keras.layers.Dense(10, activation='softmax')
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Flatten(input_shape=(28, 28)),
+  tf.keras.layers.Dense(512, activation=tf.nn.relu),
+  tf.keras.layers.Dropout(0.2),
+  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 ])
-
-model.compile(optimizer='adam',loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
 model.fit(x_train, y_train, epochs=5)
+model.evaluate(x_test, y_test)

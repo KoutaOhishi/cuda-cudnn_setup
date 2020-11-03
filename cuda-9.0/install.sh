4abbbd9eef450408cf83970480751ebea11e330c
Kouta_Ohishi <bigbasketball568@yahoo.co.jp>
2020-11-03 11:18:51 +0900

    update: cuda-9-0 to cuda-toolkit-9-0

--- cuda-9.0/install.sh
+++ cuda-9.0/install.sh
@@ -6,13 +6,16 @@
 
 echo "=== Install CUDA9.0 ==="
 #https://qiita.com/JeJeNeNo/items/b30597918db3781e20cf
+sudo apt remove cuda-*
+sudo apt purge cuda-*
 sudo dpkg -i cuda-repo-ubuntu1604_9.0.*_amd64.deb
 sudo apt-key adv --fetch-keys http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/7fa2af80.pub
 sudo apt update
-sudo apt install cuda-9-0
+#sudo apt install cuda-9-0
+sudo apt install cuda-toolkit-9-0
 
-#sudo echo "# cuda path " >> ~/.bashrc
-#sudo echo 'export PATH="/usr/local/cuda/bin:$PATH"' >> ~/.bashrc
-#sudo echo 'export LD_LIBRARY_PATH="/usr/local/cuda/lib64:$LD_LIBRARY_PATH"' >> ~/.bashrc
-#sudo echo 'export PATH="/usr/local/cuda-9.0/bin:$PATH"' >> ~/.bashrc
-#sudo echo 'export LD_LIBRARY_PATH="/usr/local/cuda-9.0/lib64:$LD_LIBRARY_PATH"' >> ~/.bashrc
+sudo echo "# cuda path " >> ~/.bashrc
+sudo echo 'export PATH="/usr/local/cuda/bin:$PATH"' >> ~/.bashrc
+sudo echo 'export LD_LIBRARY_PATH="/usr/local/cuda/lib64:$LD_LIBRARY_PATH"' >> ~/.bashrc
+sudo echo 'export PATH="/usr/local/cuda-9.0/bin:$PATH"' >> ~/.bashrc
+sudo echo 'export LD_LIBRARY_PATH="/usr/local/cuda-9.0/lib64:$LD_LIBRARY_PATH"' >> ~/.bashrc
